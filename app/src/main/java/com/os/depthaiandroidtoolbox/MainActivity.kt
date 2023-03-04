4bc70de575907e5dd57f0125bb7fb1963b21053b
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-03-04 22:37:23 +0300

    feat: ✨ adding blob models and model path as var
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- app/src/main/java/com/os/depthaiandroidtoolbox/MainActivity.kt
+++ app/src/main/java/com/os/depthaiandroidtoolbox/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.os.depthaiandroidtoolbox
 
 import android.content.res.AssetManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.os.depthaiandroidtoolbox.databinding.ActivityMainBinding
 
@@ -9,6 +11,23 @@ import com.os.depthaiandroidtoolbox.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
+    private val yolov3_model_path = "yolo-v3-tiny-tf.blob"
+    private val yolov4_model_path = "yolov4_tiny_coco_416x416_6shave.blob"
+    private val yolov5_model_path = "yolov5s_416_6shave.blob"
+    private val mobilenet_model_path = "mobilenet-ssd.blob"
+
+    private val rgbImageView: ImageView? = null
+    private var depthImageView:android.widget.ImageView? = null
+    private val rgb_image: Bitmap? = null
+    private var depth_image:android.graphics.Bitmap? = null
+
+    private val rgbWidth = 416
+    private val rgbHeight = 416
+    private val disparityWidth = 640
+    private val disparityHeight = 400
+    private val framePeriod = 30
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
