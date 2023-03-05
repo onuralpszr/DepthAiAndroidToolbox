1abe781a2088e11c611625bb4595d472e9e4ed92
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-03-05 03:08:53 +0300

    feat: ✨ initial kotlin based camera run via NDK/depthai-core
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- app/src/main/java/com/os/depthaiandroidtoolbox/MainActivity.kt
+++ app/src/main/java/com/os/depthaiandroidtoolbox/MainActivity.kt
@@ -3,49 +3,97 @@ package com.os.depthaiandroidtoolbox
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.widget.ImageView
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.os.depthaiandroidtoolbox.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
-    private val yolov3_model_path = "yolo-v3-tiny-tf.blob"
-    private val yolov4_model_path = "yolov4_tiny_coco_416x416_6shave.blob"
-    private val yolov5_model_path = "yolov5s_416_6shave.blob"
-    private val mobilenet_model_path = "mobilenet-ssd.blob"
-
-    private val rgbImageView: ImageView? = null
-    private var depthImageView:android.widget.ImageView? = null
-    private val rgb_image: Bitmap? = null
-    private var depth_image:android.graphics.Bitmap? = null
+    /*  private val yolov3ModelPath = "yolo-v3-tiny-tf.blob"
+        private val yolov4ModelPath = "yolov4_tiny_coco_416x416_6shave.blob"*/
+    private val yolov5ModelPath = "yolov5s_416_6shave.blob"
+    /*  private val mobilenetModelPath = "mobilenet-ssd.blob"*/
 
     private val rgbWidth = 416
     private val rgbHeight = 416
     private val disparityWidth = 640
     private val disparityHeight = 400
-    private val framePeriod = 30
+    private var framePeriod: Long = 30
 
+    private var running: Boolean = false
+    private var firstTime: Boolean = false
+    private var rgbImage: Bitmap = Bitmap.createBitmap(rgbWidth, rgbHeight, Bitmap.Config.ARGB_8888)
+    private var depthImage: Bitmap =
+        Bitmap.createBitmap(disparityWidth, disparityHeight, Bitmap.Config.ARGB_8888)
+
+    fun runDepthaiCamera() {
+        if (running) {
+            if (firstTime) {
+                // Start the device
+                startDevice(yolov5ModelPath, rgbWidth, rgbHeight)
+                firstTime = false
+            }
+            val rgb = imageFromJNI()
+            if (rgb != null && rgb.isNotEmpty()) {
+                rgbImage.setPixels(rgb, 0, rgbWidth, 0, 0, rgbWidth, rgbHeight)
+                binding.rgbImageView.setImageBitmap(rgbImage)
+            }
+            val detectionsImg = detectionImageFromJNI()
+            if (detectionsImg != null && detectionsImg.isNotEmpty()) {
+                rgbImage.setPixels(detectionsImg, 0, rgbWidth, 0, 0, rgbWidth, rgbHeight)
+                binding.rgbImageView.setImageBitmap(rgbImage)
+            }
+            val depth = depthFromJNI()
+            if (depth != null && depth.isNotEmpty()) {
+                depthImage.setPixels(
+                    depth, 0, disparityWidth, 0, 0, disparityWidth, disparityHeight
+                )
+                binding.depthImageView.setImageBitmap(depthImage)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Example of a call to a native method
-        binding.sampleText.text = ""
+        // Specify running and firstTime (to connect to device)
+        if (savedInstanceState != null) {
+            running = savedInstanceState.getBoolean("running", true);
+            firstTime = savedInstanceState.getBoolean("firstTime", true);
+        } else {
+            running = true;
+            firstTime = true;
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            runDepthaiCamera()
+        }, framePeriod)
     }
 
-    /**
-     * A native method that is implemented by the 'depthaiandroidtoolbox' native library,
-     * which is packaged with this application.
-     */
+    // Main loop where the data is obtained from the device and shown into the screen
+
+
     fun getAssetManager(): AssetManager? {
         return assets
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        running = false
+        firstTime = true
+    }
+
+    // Save the variable before completing the activity
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Write the variable with the key in the Bundle
+        outState.putBoolean("running", running)
+        outState.putBoolean("firstTime", firstTime)
     }
 
 
@@ -55,7 +103,6 @@ class MainActivity : AppCompatActivity() {
     external fun depthFromJNI(): IntArray?
 
     companion object {
-        // Used to load the 'depthaiandroidtoolbox' library on application startup.
         init {
             System.loadLibrary("depthaiandroidtoolbox")
         }
