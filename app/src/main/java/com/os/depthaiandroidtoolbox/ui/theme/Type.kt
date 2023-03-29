2900543e3b63bbf39658374bdc439cade0b86792
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-03-29 03:50:16 +0300

    fix: 🚀 type kt package prefix fix for theme
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- app/src/main/java/com/os/depthaiandroidtoolbox/ui/theme/Type.kt
+++ app/src/main/java/com/os/depthaiandroidtoolbox/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.jetpackcompostestmat3.ui.theme
+package com.os.depthaiandroidtoolbox.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
