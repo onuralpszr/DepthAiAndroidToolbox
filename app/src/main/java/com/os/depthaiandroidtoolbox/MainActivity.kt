75461793e68c5634b28cdc2651cebcce38f5f6d6
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-03-31 12:35:50 +0300

    feat: 🚀 initial jetpack compose layout added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- app/src/main/java/com/os/depthaiandroidtoolbox/MainActivity.kt
+++ app/src/main/java/com/os/depthaiandroidtoolbox/MainActivity.kt
@@ -5,12 +5,26 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
-import com.os.depthaiandroidtoolbox.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.os.depthaiandroidtoolbox.ui.theme.DepthAiAndroidToolboxTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
 
     /*  private val yolov3ModelPath = "yolo-v3-tiny-tf.blob"
         private val yolov4ModelPath = "yolov4_tiny_coco_416x416_6shave.blob"
@@ -42,19 +56,19 @@ class MainActivity : AppCompatActivity() {
                 val rgb = imageFromJNI()
                 if (rgb != null && rgb.isNotEmpty()) {
                     rgbImage.setPixels(rgb, 0, rgbWidth, 0, 0, rgbWidth, rgbHeight)
-                    binding.rgbImageView.setImageBitmap(rgbImage)
+                    //binding.rgbImageView.setImageBitmap(rgbImage)
                 }
                 val detectionsImg = detectionImageFromJNI()
                 if (detectionsImg != null && detectionsImg.isNotEmpty()) {
                     rgbImage.setPixels(detectionsImg, 0, rgbWidth, 0, 0, rgbWidth, rgbHeight)
-                    binding.rgbImageView.setImageBitmap(rgbImage)
+                    //binding.rgbImageView.setImageBitmap(rgbImage)
                 }
                 val depth = depthFromJNI()
                 if (depth != null && depth.isNotEmpty()) {
                     depthImage.setPixels(
                         depth, 0, disparityWidth, 0, 0, disparityWidth, disparityHeight
                     )
-                    binding.depthImageView.setImageBitmap(depthImage)
+                    //binding.depthImageView.setImageBitmap(depthImage)
                 }
     }
 
@@ -69,30 +83,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
 
         // Specify running and firstTime (to connect to device)
         if (savedInstanceState != null) {
-            running = savedInstanceState.getBoolean("running", true);
-            firstTime = savedInstanceState.getBoolean("firstTime", true);
+            running = savedInstanceState.getBoolean("running", true)
+            firstTime = savedInstanceState.getBoolean("firstTime", true)
         } else {
-            running = true;
-            firstTime = true;
+            running = true
+            firstTime = true
         }
         scheduleDepthaiCamera()
 
-
-/*        Handler(Looper.getMainLooper()).postDelayed({
-            runDepthaiCamera()
-        }, framePeriod)*/
+        setContent {
+            DepthAiAndroidToolboxTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DefaultLayout(rgbImage,rgbImage)
+                }
+            }
+        }
     }
 
 
-
-    // Main loop where the data is obtained from the device and shown into the screen
-
-
+    // We need AssetManager for JNI part
     fun getAssetManager(): AssetManager? {
         return assets
     }
@@ -111,16 +129,47 @@ class MainActivity : AppCompatActivity() {
         outState.putBoolean("firstTime", firstTime)
     }
 
-
-
-    external fun startDevice(modelPath: String?, rgbWidth: Int, rgbHeight: Int)
-    external fun imageFromJNI(): IntArray?
-    external fun detectionImageFromJNI(): IntArray?
-    external fun depthFromJNI(): IntArray?
+    private external fun startDevice(modelPath: String?, rgbWidth: Int, rgbHeight: Int)
+    private external fun imageFromJNI(): IntArray?
+    private external fun detectionImageFromJNI(): IntArray?
+    private external fun depthFromJNI(): IntArray?
 
     companion object {
         init {
             System.loadLibrary("depthaiandroidtoolbox")
         }
+    }
+}
+
+@Composable
+fun BitmapImage(bitmap: Bitmap) {
+    Image(
+        modifier = Modifier.background(Color.White),
+        bitmap = bitmap.asImageBitmap(),
+        contentDescription = "DepthAi Preview Bitmap",
+    )
+}
+
+@Composable
+fun DefaultLayout(rgbBmp:Bitmap,depthBmp:Bitmap) {
+    Column(
+        modifier = Modifier.background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        // content here
+        Text(text = "Rgb Camera ")
+        BitmapImage(bitmap = rgbBmp)
+        Text(text = "YoloV5 Camera ")
+        BitmapImage(bitmap = depthBmp)
+    }
+}
+
+
+// Default Preview
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DepthAiAndroidToolboxTheme {
     }
 }
